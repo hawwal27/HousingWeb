917abcd054303ddcb042fb4d1745786c53672946
Yahaya Azeez <azconcept2016@gmail.com>
2024-12-20 00:10:44 +0100

    feature(signup): integrate backend

---
+++ js/signup.js
@@ -0,0 +1,48 @@
+const baseUrl = new URL("http://localhost:4000");
+
+document.getElementById("signup-form").addEventListener("submit", async function (event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Get form values
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirm-pass").value;
+  const agreedToTerms = document.getElementById("agreedToTerms").checked;
+
+  const user = {
+    email,
+    password,
+    confirmPassword
+  }
+
+  const signupUrl = new URL('/api/v1/user/signup', baseUrl);
+
+  let response = await fetch(signupUrl, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  })
+
+  let result = await response.json();
+
+  if (result.status === "success") {
+    alertMessage(result.message, result.status)
+  } else {
+    alertMessage(result.message, result.status)
+  }
+});
+
+function alertMessage(message, status) {
+  let div = document.createElement('div');
+  if (status === "success") {
+    div.className = "alert-success";
+  } else {
+    div.className = "alert-failure";
+  }
+  div.innerHTML = `<strong>${status}!</strong> ${message}`;
+
+  document.body.append(div);
+  setTimeout(() => div.remove(), 5000);
+}
