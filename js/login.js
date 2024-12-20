b8709d7de396108c365dab2b90e703b5c38784c8
Yahaya Azeez <azconcept2016@gmail.com>
2024-12-20 09:16:47 +0100

    feature(login): add auth token for login user

--- js/login.js
+++ js/login.js
@@ -1,3 +1,50 @@
-const form = document.querySelector('form')
+const baseUrl = new URL("http://localhost:4000");
 
-console.log(form);
+document.getElementById("login-form").addEventListener("submit", async function (event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Get form values
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const user = {
+    email,
+    password,
+  }
+
+  const loginUrl = new URL('/api/v1/user/login', baseUrl);
+
+  let response = await fetch(loginUrl, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  })
+
+  let result = await response.json();
+
+  window.localStorage.setItem('access_token', result.token);
+
+  if (result.status === "success") {
+    alertMessage(result.message, result.status)
+  } else {
+    alertMessage(result.message, result.status)
+  }
+  // const token = window.localStorage.getItem('access_token')
+
+  // window.localStorage.removeItem('access_token')
+});
+
+function alertMessage(message, status) {
+  let div = document.createElement('div');
+  if (status === "success") {
+    div.className = "alert-success";
+  } else {
+    div.className = "alert-failure";
+  }
+  div.innerHTML = `<strong>${status}!</strong> ${message}`;
+
+  document.body.append(div);
+  setTimeout(() => div.remove(), 5000);
+}
